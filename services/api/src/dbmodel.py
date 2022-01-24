7cb12e585c61a3b55d7d62d3a00c5bf012f6a66a
vishwamitra <vishwamitra01@gmail.com>
2022-01-24 23:30:33 +0100

    fixed connection between api and db

---
+++ services/api/src/dbmodel.py
@@ -0,0 +1,66 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+
+#init my cool db
+db = SQLAlchemy()
+
+#init sweet marshmallow
+ma=Marshmallow()
+
+
+# product model class
+class Product(db.Model):
+    __tablename__ = 'Product'
+    ProductID = db.Column(db.Integer, primary_key=True)
+    ProductCode = db.Column(db.String(20), unique=True)
+    ProductName = db.Column(db.String(200))
+    ProductDescription = db.Column(db.String(1000))
+    ProductSummary = db.Column(db.Text)
+    ProductPrice = db.Column(db.Float)
+    ProductVAT = db.Column(db.Float)
+
+    def __init__(self, ProductID, ProductCode, ProductName, ProductDescription, ProductSummary, ProductPrice, ProductVAT):
+        self.ProductID = ProductID
+        self.ProductCode = ProductCode
+        self.ProductName = ProductName
+        self.ProductDescription = ProductDescription
+        self.ProductSummary = ProductSummary
+        self.ProductPrice = ProductPrice
+        self.ProductVAT = ProductVAT
+
+
+# product review model class
+class Review(db.Model):
+    __tablename__ = 'Review'
+    ReviewID = db.Column(db.Integer, primary_key=True)
+    ProductID = db.Column(db.Integer, db.ForeignKey('product.ProductID'))
+    Reviewer = db.Column(db.String(100))
+    ReviewPriceQuality = db.Column(db.Integer)
+    ReviewSummary = db.Column(db.Text)
+    ReviewDescription = db.Column(db.Text)
+    ReviewTime = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __init__(self, ProductID, Reviewer, ReviewPriceQuality, ReviewSummary, ReviewDescription):
+        self.ProductID = ProductID
+        self.Reviewer = Reviewer
+        self.ReviewPriceQuality = ReviewPriceQuality
+        self.ReviewDescription = ReviewDescription
+        self.ReviewSummary = ReviewSummary
+        
+
+# Product class Schema to serealize
+class ProductSchema(ma.Schema):
+  class Meta:
+    fields = ('ProductID', 'ProductCode', 'ProductName', 'ProductDescription', 'ProductSummary', 'ProductPrice', 'ProductVAT')
+
+# Product Review class Schema to serealize
+class ProductReviewSchema(ma.Schema):
+  class Meta:
+    fields = ('ProductID', 'Reviewer', 'ReviewPriceQuality', 'ReviewDescription', 'ReviewSummary')
+
+# Initialize the schemas
+product_schema = ProductSchema() # for single prodcut
+products_schema = ProductSchema(many=True)  # for multiple products
+product_review_schema = ProductReviewSchema() # always multiple reviews
+product_reviews_schema = ProductReviewSchema(many=True) # always multiple reviews
--- services/db/schema.sql
+++ services/db/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE PRODUCT
+CREATE TABLE Product
 (
   ProductID INT NOT NULL,
   ProductCode VARCHAR(20) NOT NULL,
@@ -11,26 +11,27 @@ CREATE TABLE PRODUCT
   UNIQUE (ProductCode)
 );
 
-CREATE TABLE REVIEW
+CREATE TABLE Review
 (
-  ReviewID INT NOT NULL,
+  ReviewID INT AUTO_INCREMENT,
   ProductID INT NOT NULL,
   Reviewer VARCHAR(100) NOT NULL,
   ReviewPriceQuality INT NOT NULL,
-  ReviewUX INT NOT NULL,
   ReviewSummary VARCHAR(1000) NOT NULL,
   ReviewDescription VARCHAR(1000) NOT NULL,
-  ReviewTime DATETIME NOT NULL,
+  ReviewTime DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (ReviewID),
-  FOREIGN KEY (ProductID) REFERENCES PRODUCT(ProductID)
+  FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
 );
 
-INSERT INTO PRODUCT
-VALUES(1,'IPX2020','iPhone X','This is the DESCRITION','Summary',999.0,9.0);
-INSERT INTO PRODUCT
-VALUES(2,'IPX2021','iPhone X-II','This is a DESCRITION','Summary',1000.0, 10.0);
-INSERT INTO REVIEW
-VALUES(1,1,'Joe',7,8,'summary','description','2020-01-01 12:00:00');
+INSERT INTO Product VALUES(1,'IPX2021','iPhone X','This is the DESCRITION','Summary', 999.0, 9.0);
+INSERT INTO Product VALUES(2,'IPX2022','iPod','This is a DESCRITION','Summary', 250.0, 10.0);
+INSERT INTO Product VALUES(3,'IPX2023','MacBook','This is the DESCRITION','Summary', 1099.0, 9.0);
+INSERT INTO Product VALUES(4,'IPX2024','Samsung S3','This is a DESCRITION','Summary', 1000.0, 10.0);
+INSERT INTO Product VALUES(5,'IPX2025','AirPods','This is the DESCRITION','Summary', 999.0, 9.0);
+INSERT INTO Product VALUES(6,'IPX2026','Windows Laptop','This is a DESCRITION','Summary', 1000.0, 10.0);
+INSERT INTO Product VALUES(7,'IPX2027','Apple watch','This is the DESCRITION','Summary', 999.0, 9.0);
+INSERT INTO Product VALUES(8,'IPX2028','Apple Air Tag','This is a DESCRITION','Summary', 1000.0, 10.0);
 
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
 
