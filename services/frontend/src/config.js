e2197a50994f5eabf2a9e92896a9c9f0613b123b
DiederikBlanson <diederikblanson@outlook.com>
2022-01-28 17:13:38 +0100

    setup kubernetes frontend

---
+++ services/frontend/src/config.js
@@ -0,0 +1,4 @@
+const envSettings = window
+export default  {
+  api_url: envSettings.API_URL
+}
--- services/frontend/src/pages/Products.js
+++ services/frontend/src/pages/Products.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { NavLink } from 'react-router-dom'
+import config from '../config'
 
 class Component extends React.Component {
 
