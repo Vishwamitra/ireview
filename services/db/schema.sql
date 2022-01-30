80b7ab1506c4c809138d2e5664f46c6f6b3069bb
vishwamitra <vishwamitra01@gmail.com>
2022-01-30 05:59:06 +0100

    created deployments for backend

--- services/db/schema.sql
+++ services/db/schema.sql
@@ -1,25 +1,26 @@
+
+GRANT ALL PRIVILEGES ON DATABASE postgres TO postgresadmin;
 CREATE TABLE Product
 (
-  ProductID INT NOT NULL,
+  ProductID INT NOT NULL UNIQUE,
   ProductCode VARCHAR(20) NOT NULL,
   ProductName VARCHAR(200) NOT NULL,
   ProductDescription VARCHAR(1000) NOT NULL,
   ProductSummary VARCHAR(1000) NOT NULL,
   ProductPrice FLOAT NOT NULL,
   ProductVAT FLOAT NOT NULL,
-  PRIMARY KEY (ProductID),
-  UNIQUE (ProductCode)
+  PRIMARY KEY (ProductID)
 );
 
 CREATE TABLE Review
 (
-  ReviewID INT AUTO_INCREMENT,
+  ReviewID SERIAL  NOT NULL,
   ProductID INT NOT NULL,
   Reviewer VARCHAR(100) NOT NULL,
   ReviewPriceQuality INT NOT NULL,
   ReviewSummary VARCHAR(1000) NOT NULL,
   ReviewDescription VARCHAR(1000) NOT NULL,
-  ReviewTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+  ReviewTime timestamp DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (ReviewID),
   FOREIGN KEY (ProductID) REFERENCES Product(ProductID)
 );
@@ -32,7 +33,3 @@ INSERT INTO Product VALUES(5,'IPX2025','AirPods','This is the DESCRITION','Summa
 INSERT INTO Product VALUES(6,'IPX2026','Windows Laptop','This is a DESCRITION','Summary', 1000.0, 10.0);
 INSERT INTO Product VALUES(7,'IPX2027','Apple watch','This is the DESCRITION','Summary', 999.0, 9.0);
 INSERT INTO Product VALUES(8,'IPX2028','Apple Air Tag','This is a DESCRITION','Summary', 1000.0, 10.0);
-
-ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
-
-flush privileges;
