219fe68fa2947c2fc2c9f93e9e3940efdf435e2a
diederik <diederik@MacBook-Pro-2015.local>
2022-02-03 17:50:19 +0100

    setup roles

---
+++ architecture/rbac/create-roles.sh
@@ -0,0 +1,4 @@
+kubectl apply -f all-namespaces-pod-reader-role.yaml
+kubectl apply -f all-namespaces-pod-reader.yaml
+kubectl apply -f namespace-pod-reader-role.yaml
+kubectl apply -f namespace-pod-reader.yaml
--- install.sh
+++ install.sh
@@ -11,3 +11,5 @@ echo "installing front end now..."
 helm install frontend ./architecture/frontend/helm/frontend -n ireview-app
 
 echo " all installation done"
+
+# TODO: install roles
