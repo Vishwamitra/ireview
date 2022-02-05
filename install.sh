7fcb614e5a8938e45828ce39bb637d06cfe8acb9
XU-Nuo <xunuo_promise@outlook.com>
2022-02-05 01:26:25 +0100

    Tweak for GKE

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 echo "Installing postgres DB..."
 
-sudo microk8s helm3 install postgres ./architecture/db/helm/postgres/ -n ireview-app
+helm install postgres ./architecture/db/helm/postgres/ -n ireview-app
 
 echo "Installing Api...."
 
-sudo microk8s helm3 install api ./architecture/api/helm/api -n ireview-app
+helm install api ./architecture/api/helm/api -n ireview-app
 
 echo "installing front end now..."
 
-sudo microk8s helm3 install frontend ./architecture/frontend/helm/frontend -n ireview-app
+helm install frontend ./architecture/frontend/helm/frontend -n ireview-app
 
 echo " all installation done"
